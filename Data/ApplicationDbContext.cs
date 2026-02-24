f5177bad16065017db1f61d25993afb7cc14a0e3
bortiz-101 <bortiz101@hotmail.com>
2026-02-23 21:38:28 -0600

    refactors from webapp to console app

--- Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Community_Event_Finder.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
-    {
-    }
-}
