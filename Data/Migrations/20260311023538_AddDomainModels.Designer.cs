2e6239a62aa30d9df86b02d6f541d4bf7331d583
bortiz-101 <bortiz101@hotmail.com>
2026-03-10 20:24:37 -0700

    add migration for domain models

--- Data/Migrations/20260311023538_AddDomainModels.Designer.cs
+++ Data/Migrations/20260311023538_AddDomainModels.Designer.cs
@@ -4,6 +4,7 @@ using Community_Event_Finder.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Community_Event_Finder.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260311023538_AddDomainModels")]
+    partial class AddDomainModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
